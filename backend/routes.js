93bf730acb11d1f11c246c9be8eeca06b8aff361
Michael Crabtree <622michael@gmail.com>
2017-12-18 23:54:08 -0500

    added sequelize as the Postgres ORM

--- backend/routes.js
+++ backend/routes.js
@@ -3,9 +3,20 @@ const app = express()
 
 const example = require("./example")
 
+const models = require("./db/models")
+
 // All routes defined here
 
 app.get('/', example.data.printHello)
 app.get('/user/:name', example.data.printName)
 
-app.listen(3000, () => console.log("WOAH WOAH WOAH"))
+
+models.sequelize.authenticate().then(function(err) {
+	// Connected to database succesfully
+
+	app.listen(3000, () => console.log("WOAH WOAH WOAH"))
+}).catch(function(err) {
+	// Failed to connect to database
+
+	console.log("Failure to connect to database:", err)
+});
