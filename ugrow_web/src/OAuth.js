ad3ee95633134dbd743ba5f210f120e293cc43e2
musa jamshed <Musa@musas-Air.lan>
2017-12-26 19:44:34 -0500

    Switched up the OAuth library, adding ability to get id_token after login

--- ugrow_web/src/OAuth.js
+++ ugrow_web/src/OAuth.js
@@ -1,38 +1,31 @@
 import React from 'react';
-import {GoogleAPI,GoogleLogin,GoogleLogout} from 'react-google-oauth'
+import {GoogleLogin, GoogleLogout} from 'react-google-login';
  
 export default class GoogleAuth extends React.Component {
 
     //WEB_CLIENT_ID = "520933852537-7kp1tqiq1jl8qddoj9vsoh8rvu2a1tha.apps.googleusercontent.com";
 
-    constructor(props) {
-        super(props);
-        this.state = {
-          isLoggedIn: false
-        };
-    }
+    /* Collects the response */
+    responseGoogle(response) {
+        // Grabs the portion we want:
+        //  - Right now it's the whole token object
+        //  - If you want to get further though, use the console (on Google Chrome is what I did) to see the object hierarchy
+        //  - i.e. I think adding '.id_token' to the end of this is what we need but I'm not sure
+        let theGoods = response.tokenObj;
 
-    toggle() {
-        this.setState({
-          isLoggedIn: !this.state.isLoggedIn
-        });
-    }
+        // Here is where it logs it, again check it out because you can see the entire hierarchy
+        console.log(theGoods);
+      }
 
     render() {
         return (
-            <GoogleAPI clientId = "520933852537-7kp1tqiq1jl8qddoj9vsoh8rvu2a1tha.apps.googleusercontent.com"
-                onUpdateSigninStatus={Function}
-                onInitFailure={Function} >
-                <div>
-                    { this.state.isLoggedIn ? <GoogleLogout
-                                                text = "Log out boi"
-                                                onLogoutSuccess = {() => this.toggle()}/> 
-                                            : <GoogleLogin 
-                                                text = "Log in PLEASE"
-                                                onLoginSuccess = {() => this.toggle()}/> 
-                    }
-    		    </div>
-            </GoogleAPI>
+                // The whole thing is just a button, with the functionality included
+                // NOTE: the button is kind of ugly but can deal with that part later
+                <GoogleLogin 
+                    clientId="520933852537-7kp1tqiq1jl8qddoj9vsoh8rvu2a1tha.apps.googleusercontent.com"
+                    buttonText="Login boii"
+                    onSuccess={this.responseGoogle}
+                    onFailure={this.responseGoogle} />
         );
     }
 }
