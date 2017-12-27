143f54bc2a5823efd1e8027f6c30dcf52f7661b0
musa jamshed <Musa@musas-Air.lan>
2017-12-27 14:58:07 -0500

    Refactored OAuth with better notation

--- ugrow_web/src/OAuth.js
+++ ugrow_web/src/OAuth.js
@@ -1,59 +1,58 @@
 import React from 'react';
 import {GoogleLogin, GoogleLogout} from 'react-google-login';
  
-var isLoggedIn = false;
 export default class GoogleAuth extends React.Component {
 
-    /* constructor(props) {
+    constructor(props) {
         super(props);
         this.state = {
             isLoggedIn: false
         }
-    } */
-    // = "520933852537-7kp1tqiq1jl8qddoj9vsoh8rvu2a1tha.apps.googleusercontent.com";
-
-    /* Collects the response */
-    responseGoogle(response) {
-        // Grabs the portion we want:
-        //  - Right now it's the whole token object
-        //  - If you want to get further though, use the console to see the object hierarchy
-        //  - You were right, musa
-        let theGoods = response.tokenObj.id_token;
-
-        isLoggedIn = true;
-
-        // Here is where it logs it, again check it out because you can see the entire hierarchy
-        console.log(theGoods);
     }
 
-    // currently response is always false, so either the logout doesn't work or onLogoutSuccess does not
-    loggedOut(response) {
-        isLoggedIn = false;
-        response ? alert("You logged out") : alert("you did not log out");
+    //const WEB_CLIENT_ID = '520933852537-7kp1tqiq1jl8qddoj9vsoh8rvu2a1tha.apps.googleusercontent.com';
+
+    /* Collects the response */
+
+    handleLoginSuccess = (response) => {
+        // Grabs the token portion we want:
+        let theGoods = response.tokenObj.id_token;
+        console.log(theGoods);
+
+        // Sets state to logged in
+        this.setState({
+            isLoggedIn: true
+        })
+    }
+
+    handleLogoutSuccess = () => {
+        // NOTE: there is no response
+        // Sets state to logged out
+        this.setState({
+            isLoggedIn: false
+        })
     }
 
     render() {
-        if (!isLoggedIn){
-            /* this.setState({
-                isLoggedIn: !this.state.isLoggedIn
-            }); */
+        if (!this.state.isLoggedIn){
+            // Render as Login button
             return (
                     // The whole thing is just a button, with the functionality included
                     // NOTE: the button is kind of ugly but can deal with that part later 
                     <GoogleLogin 
                         clientId="520933852537-7kp1tqiq1jl8qddoj9vsoh8rvu2a1tha.apps.googleusercontent.com"
-                        buttonText="Login boii"
-                        onSuccess={this.responseGoogle}
+                        buttonText="Login!"
+                        onSuccess={this.handleLoginSuccess}
                         onFailure={this.responseGoogle} />   
             );
         }
+
         else{
+            // Render as Logout button
             return (
                 <GoogleLogout
-                    buttonText="Logout boi or gurl or other"
-                    onLogoutSuccess={this.loggedOut}
-                    >
-                </GoogleLogout>
+                    buttonText="Logout!"
+                    onLogoutSuccess={this.handleLogoutSuccess} />
             );
         }
     }
