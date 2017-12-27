9f2996e67c2eca4eb2f1a0e4dd16da516d8ea532
Tommy <tev219@lehigh.edu>
2017-12-27 11:45:21 -0500

    toggle login/logout button (but not using googd practice)

--- ugrow_web/src/OAuth.js
+++ ugrow_web/src/OAuth.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import {GoogleLogin, GoogleLogout} from 'react-google-login';
  
+var isLoggedIn = false;
 export default class GoogleAuth extends React.Component {
 
-    //WEB_CLIENT_ID = "520933852537-7kp1tqiq1jl8qddoj9vsoh8rvu2a1tha.apps.googleusercontent.com";
+    /* constructor(props) {
+        super(props);
+        this.state = {
+            isLoggedIn: false
+        }
+    } */
+    // = "520933852537-7kp1tqiq1jl8qddoj9vsoh8rvu2a1tha.apps.googleusercontent.com";
 
     /* Collects the response */
     responseGoogle(response) {
@@ -13,32 +20,41 @@ export default class GoogleAuth extends React.Component {
         //  - You were right, musa
         let theGoods = response.tokenObj.id_token;
 
+        isLoggedIn = true;
+
         // Here is where it logs it, again check it out because you can see the entire hierarchy
         console.log(theGoods);
     }
 
     // currently response is always false, so either the logout doesn't work or onLogoutSuccess does not
     loggedOut(response) {
+        isLoggedIn = false;
         response ? alert("You logged out") : alert("you did not log out");
     }
 
     render() {
-        return (
-                // The whole thing is just a button, with the functionality included
-                // NOTE: the button is kind of ugly but can deal with that part later
-
-                <div>
-                <GoogleLogin 
-                    clientId="520933852537-7kp1tqiq1jl8qddoj9vsoh8rvu2a1tha.apps.googleusercontent.com"
-                    buttonText="Login boii"
-                    onSuccess={this.responseGoogle}
-                    onFailure={this.responseGoogle} /> 
+        if (!isLoggedIn){
+            /* this.setState({
+                isLoggedIn: !this.state.isLoggedIn
+            }); */
+            return (
+                    // The whole thing is just a button, with the functionality included
+                    // NOTE: the button is kind of ugly but can deal with that part later 
+                    <GoogleLogin 
+                        clientId="520933852537-7kp1tqiq1jl8qddoj9vsoh8rvu2a1tha.apps.googleusercontent.com"
+                        buttonText="Login boii"
+                        onSuccess={this.responseGoogle}
+                        onFailure={this.responseGoogle} />   
+            );
+        }
+        else{
+            return (
                 <GoogleLogout
-                    buttonText="Logout"
+                    buttonText="Logout boi or gurl or other"
                     onLogoutSuccess={this.loggedOut}
                     >
-                </GoogleLogout>    
-                </div>     
-        );
+                </GoogleLogout>
+            );
+        }
     }
 }
