1faf1e251adf5623c1839d71a1678f7bed8dce33
Tommy <tev219@lehigh.edu>
2017-12-26 22:18:02 -0500

    added logout button (though funcionality unclear as well as no toggle) and confirmed reception of id_token (what is to be sent to server)

--- ugrow_web/src/OAuth.js
+++ ugrow_web/src/OAuth.js
@@ -9,23 +9,36 @@ export default class GoogleAuth extends React.Component {
     responseGoogle(response) {
         // Grabs the portion we want:
         //  - Right now it's the whole token object
-        //  - If you want to get further though, use the console (on Google Chrome is what I did) to see the object hierarchy
-        //  - i.e. I think adding '.id_token' to the end of this is what we need but I'm not sure
-        let theGoods = response.tokenObj;
+        //  - If you want to get further though, use the console to see the object hierarchy
+        //  - You were right, musa
+        let theGoods = response.tokenObj.id_token;
 
         // Here is where it logs it, again check it out because you can see the entire hierarchy
         console.log(theGoods);
-      }
+    }
+
+    // currently response is always false, so either the logout doesn't work or onLogoutSuccess does not
+    loggedOut(response) {
+        response ? alert("You logged out") : alert("you did not log out");
+    }
 
     render() {
         return (
                 // The whole thing is just a button, with the functionality included
                 // NOTE: the button is kind of ugly but can deal with that part later
+
+                <div>
                 <GoogleLogin 
                     clientId="520933852537-7kp1tqiq1jl8qddoj9vsoh8rvu2a1tha.apps.googleusercontent.com"
                     buttonText="Login boii"
                     onSuccess={this.responseGoogle}
-                    onFailure={this.responseGoogle} />
+                    onFailure={this.responseGoogle} /> 
+                <GoogleLogout
+                    buttonText="Logout"
+                    onLogoutSuccess={this.loggedOut}
+                    >
+                </GoogleLogout>    
+                </div>     
         );
     }
 }
